23e3aaf64346e2a3403949ebfbdfe4cd5eb9b231
Ejazkhan42 <karaechi420@gmail.com>
2024-12-18 22:04:37 +0500

    New Version

--- build/env.js
+++ build/env.js
@@ -1,5 +1,5 @@
 window.env = {
-            AP: "http://localhost:5000/api",
+            AP: "http://localhost:5000/ap",
             VP: "c2VsZW5vaWQ=",
             VU: "https://gridview.doingerp.com:443"
 };
