27a1943af9e22cfbd15df0e03b0dc182bd9dedc2
Ejazkhan42 <karaechi420@gmail.com>
2025-01-08 18:37:54 +0500

    UPDATES

--- src/app.jsx
+++ src/app.jsx
@@ -42,27 +42,27 @@ const AppRoutes = () => {
             <AdminRoute />
           </PrivateRoute>
         }
-      > 
-        
+      >
+
         <Route path='/' element={<Homepage pathname={router.pathname} navigate={router.navigate} />} />
-        <Route path="/dashboard" element={<Homepage pathname={router.pathname} navigate={router.navigate}/>} />
+        <Route path="/dashboard" element={<Homepage pathname={router.pathname} navigate={router.navigate} />} />
         <Route path="/home" element={<Homepage pathname={router.pathname} navigate={router.navigate} />} />
-        <Route path='/Instances' element={<Instance pathname={router.pathname} navigate={router.navigate}/>} />
-        <Route path='/customers' element={<Customers pathname={router.pathname} navigate={router.navigate}/>} />
+        <Route path='/Instances' element={<Instance pathname={router.pathname} navigate={router.navigate} />} />
+        <Route path='/customers' element={<Customers pathname={router.pathname} navigate={router.navigate} />} />
         <Route path='/business/Scenario' element={<Scenario_manager />} />
         <Route path='/business/objects' element={<Objects pathname={router.pathname} navigate={router.navigate} />} />
         <Route path='/env' element={<Env pathname={router.pathname} navigate={router.navigate} />} />
         <Route path='/Modules' element={<Modules pathname={router.pathname} navigate={router.navigate} />} />
-        <Route path='/progress' element={<Progress pathname={router.pathname} navigate={router.navigate}/>} />
+        <Route path='/progress' element={<Progress pathname={router.pathname} navigate={router.navigate} />} />
         <Route path='/Jobs' element={<TestCasePage pathname={router.pathname} navigate={router.navigate} />} />
         <Route path='/business/manager' element={<FlowPage pathname={router.pathname} navigate={router.navigate} />} />
         <Route path='/business/Components' element={<ComponentPage pathname={router.pathname} navigate={router.navigate} />} />
         <Route path='/business' element={<FlowPage pathname={router.pathname} navigate={router.navigate} />} />
         <Route path='/business/Testcase' element={<Testcase pathname={router.pathname} navigate={router.navigate} />} />
-        <Route path='/business/command' element={<Cammands  pathname={router.pathname} navigate={router.navigate}/>} />
+        <Route path='/business/command' element={<Cammands pathname={router.pathname} navigate={router.navigate} />} />
         <Route path='/business/types' element={<Types pathname={router.pathname} navigate={router.navigate} />} />
-        <Route path='/AdminPanel' element={<AdminPanel  pathname={router.pathname} navigate={router.navigate}/>} />
-        <Route path='/Setting/Modules' element={<Module  pathname={router.pathname} navigate={router.navigate}/>} />
+        <Route path='/AdminPanel' element={<AdminPanel pathname={router.pathname} navigate={router.navigate} />} />
+        <Route path='/Setting/Modules' element={<Module pathname={router.pathname} navigate={router.navigate} />} />
         <Route path='/worklist' element={<Worklist pathname={router.pathname} navigate={router.navigate} />} />
         <Route path='/Setting/Menu' element={<Menu pathname={router.pathname} navigate={router.navigate} />} />
         <Route path='/Setting' element={<Module pathname={router.pathname} navigate={router.navigate} />} />
