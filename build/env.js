0424cdbb1e6f9f04c22a0facb1562b31e8cec214
EjazApple <ejaz.ullah1@icloud.com>
2025-02-05 04:54:16 +0500

    G

--- build/env.js
+++ build/env.js
@@ -1,6 +1,7 @@
+const hostname = window.location.hostname;
+
+// Set environment variables based on hostname
 window.env = {
-            AP: "https://oracle.doingerp.com/api",
-            VP: "c2VsZW5vaWQ=",
-            VU: "https://gridview.doingerp.com:443",
-            WS: "wss://websocket.doingerp.com:443"
+  AP: `https://${hostname}/api`,  // API URL
+  WS: `wss://${hostname}`,       // WebSocket URL
 };
--- public/env.js
+++ public/env.js
@@ -1,6 +1,4 @@
 window.env = {
             AP: "https://oracle.doingerp.com/api",
-            VP: "c2VsZW5vaWQ=",
-            VU: "https://gridview.doingerp.com:443",
-            WS: "wss://websocket.doingerp.com:443"
+            WS: "wss://oracle.doingerp.com:443"
 };
